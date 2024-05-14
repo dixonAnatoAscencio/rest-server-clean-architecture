d5866655e81cf54d2d182cc682fbe17a4451de15
dixonAnatoAscencio <dixonjesusanato@gmail.com>
2024-05-14 13:42:18 -0500

    mail service

--- src/presentation/services/auth.service.ts
+++ src/presentation/services/auth.service.ts
@@ -29,9 +29,12 @@ export class AuthService {
 
       const { password, ...userEntity } = UserEntity.fromObject(user);
 
+      const token = await JwtAdapter.generateToken({ id: user.id });
+      if (!token) throw CustomError.internalServer("Error while creating JWT");
+
       return {
         user: userEntity,
-        token: "ABC",
+        token: token,
       };
     } catch (error) {
       throw CustomError.internalServer(`${error}`);
@@ -50,10 +53,7 @@ export class AuthService {
 
     const { password, ...userEntity } = UserEntity.fromObject(user);
 
-    const token = await JwtAdapter.generateToken({
-      id: user.id,
-      email: user.email,
-    });
+    const token = await JwtAdapter.generateToken({ id: user.id });
     if (!token) throw CustomError.internalServer("Error while creating JWT");
 
     return {
